0394fdcab6695fabe29d1253f67b6e55e57fb617
jeevasaas <jeevanantham@saasforest.com>
2023-12-12 14:36:12 +0530

    bugs

--- resources/views/tables/columns/copy.blade.php
+++ resources/views/tables/columns/copy.blade.php
@@ -1,10 +1,10 @@
-<div>
-    <div x-data="{ jeeva: '' }" x-init="jeeva = '{{ $getState() }}'">
-        <button x-on:click="navigator.clipboard.writeText(jeeva).then(() => { 
+{{-- <div> --}}
+    {{-- <div x-data="{ jeeva: '' }" x-init="jeeva = '{{ $getState() }}'"> --}}
+        {{-- <button x-on:click="navigator.clipboard.writeText(jeeva).then(() => { 
             alert('Copied'); 
-        })">
+        })"> --}}
         {{-- {{ $getState() }} --}}
-        <span x-text="'*****' + jeeva.substring(5)"> </span>
-        </button>
-    </div>
-</div>
+        {{-- <span x-text="'*****' + jeeva.substring(5)"> </span> --}}
+        {{-- </button> --}}
+    {{-- </div>
+</div> --}}
